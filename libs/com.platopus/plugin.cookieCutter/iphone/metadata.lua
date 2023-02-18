1c0b671fb864524bfe42915427275c8208c9c5d7
yousaf-shah <59312216+yousaf-shah@users.noreply.github.com>
2023-02-18 15:14:17 +0000

    Added Compiled Libraries

---
+++ libs/com.platopus/plugin.cookieCutter/iphone/metadata.lua
@@ -0,0 +1,20 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+
+		-- This is the name without the "lib" prefix
+        -- In this case, the static library is called "libplugin_openudid.a" should be "plugin_openudid"
+		staticLibs = { 'plugin_cookieCutter', },
+		frameworks = {},
+		frameworksOptional = {},
+	},
+    coronaManifest = {
+        dependencies = {
+            -- ["shared.memoryBitmap"] = "com.coronalabs",
+        },
+    },
+}
+
+return metadata
